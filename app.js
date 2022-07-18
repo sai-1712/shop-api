7819d69632a9601530fd58fc8d8b1fc55302493b
Sai Kiran <ravirala.sai1712@gmail.com>
2022-07-18 21:17:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+const userRoutes = require('./api/routes/user');
+
+// To Connect using mongo Atlas
+
+mongoose.connect(
+  "mongodb+srv://node-shop:"+ process.env.MONGO_ATLAS_PW +"@node-rest-shop.xosa8.mongodb.net/?retryWrites=true&w=majority"
+);
+
+// To Connect using mongoDb Compass
+
+// mongoose.connect("mongodb://localhost:27017/nodeshop", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connection to MONGO successful");
+//   })
+//   .catch((err) => {
+//     console.log("error connecting to mongo");
+//   });
+
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
